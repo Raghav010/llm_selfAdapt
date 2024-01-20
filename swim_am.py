b22f8562662542eb2dd76689438bedbdaad69f8e
raghav <raghavdonakanti@gmail.com>
2024-01-20 16:32:45 +0530

    run1 completed

--- swim_am.py
+++ swim_am.py
@@ -1,6 +1,10 @@
 from socket import socket, AF_INET, SOCK_STREAM
 import time
 from time import sleep
+from openai import OpenAI
+from dotenv import load_dotenv
+
+load_dotenv()
 
 
 # the swim interface, gets and sets data on SWIM
@@ -95,7 +99,7 @@ class SwimInterface:
 
     def get_total_utilization(self):
         utilization = 0
-        active_servers = self.get_active_servers()
+        active_servers = int(self.get_active_servers())
         for s in range(1, active_servers + 1):
             utilization += self.get_utilization(s)
         return utilization
@@ -115,11 +119,11 @@ class SwimInterface:
         if commandNo==0:
             return self.get_dimmer()
         elif commandNo==1:
-            return self.get_servers()
+            return self.set_dimmer(float(args))
         elif commandNo==2:
-            return self.get_active_servers()
+            return self.add_server()
         elif commandNo==3:
-            return self.get_max_servers()
+            return self.remove_server()
         elif commandNo==4:
             return self.get_utilization(int(args))
         elif commandNo==5:
@@ -133,11 +137,11 @@ class SwimInterface:
         elif commandNo==9:
             return self.get_arrival_rate()
         elif commandNo==10:
-            self.add_server()
+            return self.get_active_servers()
         elif commandNo==11:
-            self.remove_server()
+            return self.get_max_servers()
         elif commandNo==12:
-            self.set_dimmer(float(args))
+            return self.get_servers()
         elif commandNo==13:
             return self.get_total_utilization()
         elif commandNo==14:
@@ -182,27 +186,81 @@ class SwimInterface:
 #     print(swimClient.choose_command(cmdNo,args))
 
 
-class LLMInterface():
-    def __init__(self,api_key,instruction,few_shot,closing):
-        # connect to the LLM api service
-        self.instruction=instruction
-        self.few_shot=few_shot
-        self.closing=closing
+# handles history and keeping track of the chat, handles context overflow
+class Chat():
+    def __init__(self,maxContext,client):
+        self.chat=[]
+        self.maxContext=maxContext
+        self.client=client
+    
+    def condenseHistory(self):
+        self.chat.append({"role":"user","content":'Summarize the conversation till now'})
+        res=self.client.create(
+            model='gpt-4',
+            messages=self.chat #add max tokens?
+        )
+        self.chat=[{"role":"user","content":res.choices[0].message.content}]
+    
+    def updateChat(self,role,content,tokenCount=0):
+        self.chat.append({"role":role,"content":content})
         
+        if tokenCount > self.maxContext:
+            des=input('Max context limit reached, wanna condense? y/n')
+            if des=='y':
+                self.condenseHistory()
+
+        print(f'{role}--->{content}')
+
+
+
+class LLMInterface():
+
+
+    def __init__(self,instruction,few_shot,closing):
+        # connect to the LLM api service
+        client=OpenAI().chat.completions
+        print('Connected to the LLM api service')
+
+        self.chat=Chat(7500,client)
+
+        self.client=client
+        self.closing=closing
+
+        # send the instruction and few shot to the LLM
+        self.chat.updateChat("system",instruction)
+
+       
+        for idx,l in enumerate(few_shot):
+            if idx%2==0:
+                self.chat.updateChat("user",l)
+            else:
+                self.chat.updateChat("assistant",l)
+
+
+        res=client.create(
+            model='gpt-4',
+            messages=self.chat.chat
+        )
+
+        self.chat.updateChat('assistant',res.choices[0].message.content)
+
+    
+    # here new needs to be a list of strings
     def set_few_shot(self,new):
         self.few_shot=new
     
     def set_instruction(self,new):
         self.instruction=new
     
-    # include instruction and few shot in every prompt??
+    
     def construct_prompt(self,status):
         status_string="Status:\n"
         for var_name,stat in status.items():
             status_string+=f'{var_name}: {stat}\n'
         status_string+=self.closing
-        return f'{self.instruction}\n\n{self.few_shot}\n\n{status_string}'
+        return f'{status_string}'
     
+
     def parse_response(self,res):
         res=res.split(" ")
         cmdNo=int(res[0])
@@ -211,36 +269,40 @@ class LLMInterface():
         else:
             args=None
         return (cmdNo,args)
-        # parse the response and return a number/action to take
-        # return sim_over if the simulation is over
+
 
     # status is a dictionary of {variable_name:value}
     def ask(self,status):
         prompt=self.construct_prompt(status)
-        # send to api
-        # parse and return response
+        self.chat.updateChat("user",prompt)
+        res=self.client.create(
+            model='gpt-4',
+            messages=self.chat.chat #add max tokens?
+        )
+        self.chat.updateChat('assistant',res.choices[0].message.content,res.usage.total_tokens)
+        return self.parse_response(res.choices[0].message.content)
         
 
 
-
-api_key=None
 
 # change the system objective?
 # remove MAPE?
 # change set_dimmer to increase_dimmer and decrease_dimmer?
 
 
-instruction='''You are an adpatation manager for a server system handling user requests. Self-adaptive systems are capable of modifying their runtime behavior in order to achieve system objectives. Here the system objective is to keep the average response time as low as possible. You being the adapatation manager are responsible for modifying the runtime behavior of the system using the MAPE (Monitor-Analyze-Plan-Execute) model. You will be provided data on the status of the system and you need to analyze to decide if adaption is required and what type of adaptation is needed. The possible actions you can take to modify the system are adding a server, removing a server and changing the dimmer value.
-Terminology: 
+instruction='''You are an adpatation manager for a server system handling user requests. Self-adaptive systems are capable of modifying their runtime behavior in order to achieve system objectives. Here the system objective is to keep the average response time as low as possible. You being the adapatation manager are responsible for modifying the runtime behavior of the system using the MAPE (Monitor-Analyze-Plan-Execute) model. You will be provided data on the status of the system and you need to analyze to decide if adaption is required and what type of adaptation is needed. The possible actions you can take to modify the system are adding a server, removing a server and changing the dimmer value.'''
+
+few_shot=['''Terminology: 
 dimmer - used to control the proportion of responses that include the optional content, with 1 being the setting in which all responses include the optional content, 0 when no one does (i.e., blackout). The value of the dimmer can be thought of as the probability of a response including the optional content, thus taking values in [0..1].
 active_servers - the number of servers that are currently active and processing requests
 max_servers - the maximum number of servers that can be active at any time
 utilization - the proportion of time that all servers are busy processing requests
 average_response_time - the average time taken to process a request
 arrival_rate - the average number of requests arriving per second
-'''
+time - the seconds elapsed since the start of the system
 
-few_shot='''Here are a few examples on how you are to interact with the system. Respond with the action number. If the action has a parameter like set dimmer respond with action number followed by the dimmer value.
+                    
+Here are a few examples on how you are to interact with the system. Respond with the action number. If the action has a parameter like set dimmer respond with action number followed by the dimmer value.
 
 
 Status:
@@ -250,6 +312,7 @@ max_servers: 3
 utilization: 0.0611121
 average_response_time: 0.07638993128912003
 arrival_rate: 11.3332
+time: 0
 
 Actions you can take:
 1. Set dimmer
@@ -257,16 +320,17 @@ Actions you can take:
 3. Remove server
 4. Do nothing
 
-Answer: 
-2
+Answer:''', 
+'''2''',
 
-Status:
+'''Status:
 dimmer: 0.9
 active servers: 3
 max servers: 3
 utilization: 0.779628
 average response time: 0.07346221086027917
 arrival rate: 10.6167
+time: 200
 
 Actions you can take:
 1. Set dimmer
@@ -274,16 +338,17 @@ Actions you can take:
 3. Remove server
 4. Do nothing
 
-Answer: 
-1 0.5
+Answer:''', 
+'''1 0.5''',
 
-Status:
+'''Status:
 dimmer: 0.5
 active servers: 3
 max servers: 3
 utilization: 0.517234
 average response time: 0.05673555607704635
 arrival rate: 9.1
+time: 400
 
 Actions you can take:
 1. Set dimmer
@@ -291,16 +356,17 @@ Actions you can take:
 3. Remove server
 4. Do nothing
 
-Answer: 
-1 0.2
+Answer:''', 
+'''1 0.2''',
 
-Status:
+'''Status:
 dimmer: 0.2
 active servers: 3
 max servers: 3
 utilization: 0.39712899999999995
 average response time: 0.040725613054705435
 arrival rate: 9.76667
+time: 600
 
 Actions you can take:
 1. Set dimmer
@@ -308,9 +374,8 @@ Actions you can take:
 3. Remove server
 4. Do nothing
 
-Answer: 
-4
-'''
+Answer:''', 
+'''4''']
 
 closing='''Actions you can take:
 1. Set dimmer
@@ -326,17 +391,19 @@ Answer:'''
 swimClient=SwimInterface("127.0.0.1",4242)
 if swimClient.socket==None:
     exit(0)
-llmInterface=LLMInterface("api_key",instruction,few_shot,closing)
+llmInterface=LLMInterface(instruction,few_shot,closing)
 
 
+print(f'\n\n\nAdaptation manager starting\n\n\n')
 alive=swimClient.get_dimmer()
 start=time.time()
 while alive!='Error':
     status=swimClient.choose_command(15)
-    status['time']=time.time()-start
+    status['time']=int(time.time()-start)
     res=llmInterface.ask(status)
-    swimClient.choose_command(res[0],res[1])
-    alive=swimClient.get_dimmer() # checking if simulation is still running
+    if res[0]!=4:
+        alive=swimClient.choose_command(res[0],res[1])
+        print(f'SWIM--->{alive}')
     sleep(200)
 
 
